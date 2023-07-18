e1c4737f63fca0b8be9aec0cbaaec7ae58da4705
IsaacAguiar <126144691+IsaacAguiar@users.noreply.github.com>
2023-07-18 06:10:00 -0300

    Mentores 100% funcional com Javascript e Json

---
+++ Mentores/novoMentor.js
@@ -0,0 +1,51 @@
+const setaVoltar = () => {
+    window.location = "./mentores.html"
+}
+
+const formulario = document.getElementById('formulario')
+
+const buscarMentores = async () => {
+    const resposta = await fetch('http://localhost:3000/Mentores')
+    const mentores = await resposta.json()
+    return mentores
+}
+
+const cadastrarMentor = async (mentor) => {
+    await fetch('http://localhost:3000/Mentores', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(mentor)
+    })
+    window.location = './mentores.html'
+}
+
+formulario.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    const nome = formulario.elements['nome'].value
+    const email = formulario.elements['email'].value
+
+    const mentores = await buscarMentores()
+
+    if (mentores.length === 0) {
+        console.error('Erro: Não foi possível buscar os mentores.')
+        return
+    }
+
+    const mentorObjeto = mentores.find(mentor => mentor.nome === nome)
+
+    if (mentorObjeto) {
+        console.error('Erro: Mentor já cadastrado.')
+        return
+    }
+
+    const mentorNovo = {
+        nome,
+        email
+    }
+
+    cadastrarMentor(mentorNovo)
+})
