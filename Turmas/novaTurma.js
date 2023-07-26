9171be9aa8f0f4df0a5d2f81f823c85252bd4a2c
IsaacAguiar <126144691+IsaacAguiar@users.noreply.github.com>
2023-07-26 04:46:52 -0300

    Turmas

---
+++ Turmas/novaTurma.js
@@ -0,0 +1,81 @@
+const setaVoltar = () => {
+    window.location = "./turmas.html";
+}
+
+function preencherMentorias() {
+    fetch('http://localhost:3000/Mentorias')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Resposta da rede não foi bem-sucedida');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const selectMentoria = document.getElementById('mentoria');
+            data.forEach(mentoria => {
+                const option = document.createElement('option');
+                option.text = mentoria.titulo;
+                selectMentoria.add(option);
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao buscar Mentorias:', error);
+        });
+}
+
+function preencherMentores() {
+    fetch('http://localhost:3000/Mentores')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Resposta da rede não foi bem-sucedida');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const selectMentor = document.getElementById('mentor');
+            data.forEach(mentor => {
+                const option = document.createElement('option');
+                option.text = mentor.nome;
+                selectMentor.add(option);
+            })
+        })
+        .catch(error => {
+            console.error('Erro ao buscar Mentores:', error);
+        })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    preencherMentorias();
+    preencherMentores();
+});
+
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(document.getElementById('formulario'));
+    const formDataObj = Object.fromEntries(formData.entries());
+
+    // Unindo a hora de inicio e a hora de fim em um único campo
+    formDataObj.horario = formDataObj.horaInicio + " - " + formDataObj.horaFim;
+    delete formDataObj.horaInicio;
+    delete formDataObj.horaFim;
+
+    fetch('http://localhost:3000/Turmas', {
+        method: 'POST',
+        headers: {
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formDataObj)
+    })
+    .then(response => {
+        if (response.ok) {
+            setaVoltar();
+        } else {
+            alert('Erro ao criar turma!');
+        }
+    })
+    .catch(error => {
+        console.error('Erro ao enviar os dados:', error);
+    });
+});
